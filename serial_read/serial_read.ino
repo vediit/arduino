567fc015f453e666c898d8ea9c1873f56f04f9a8
vediit <31199071+vediit@users.noreply.github.com>
2018-01-05 12:52:05 +0530

    Add files via upload

---
+++ serial_read/serial_read.ino
@@ -0,0 +1,17 @@
+String a;
+
+void setup() {
+  Serial.begin(9600);
+
+}
+
+void loop() {
+  while (Serial.available() == 0) {}
+  int i ;
+  while (Serial.available() != 0) {
+    i = Serial.read();
+    a = a + char(i);
+    delay(10);
+  }
+  Serial.print(a);
+}
